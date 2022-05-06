6448e9196294bcaf39bce1955bbc504771e2907a
ALİ RIZA ÇELİK <riza-celik@outlook.com>
2022-05-06 23:42:36 +0300

    ı have inseted some codes for deep learning

--- fruit360.py
+++ fruit360.py
@@ -25,17 +25,16 @@ print(x.shape)
 className = glob(train_path + '/*')
 numberOfClass = len(className)
 print(f'Numberofclass', numberOfClass)
-# *************** CNN Model *********
+# ****************CNN Model***************
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=x.shape))
 model.add(MaxPooling2D())
-
-model.add(Conv2D(32,(3,3)))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation("relu"))
-model.add(MaxPooling2D())
 
-model.add(Conv2D(64,(3,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation("relu"))
 model.add(MaxPooling2D())
 
@@ -43,15 +42,71 @@ model.add(Flatten())
 model.add(Dense(1024))
 model.add(Activation("relu"))
 model.add(Dropout(0.5))
-model.add(Dense(numberOfClass))# OutputLayer
+
+model.add(Dense(numberOfClass))  # OutputLayer
 model.add(Activation("softmax"))
 
-
-model.compile(loss="categorycal_crossentropy",
+model.compile(loss="categorical_crossentropy",
               optimizer="rmsprop",
               metrics=["accuracy"]
               )
-batch_size=32
+batch_size = 32
 
-#******** Data Generation-Train-Test**********
+# ******** Data Generation-Train-Test**********
 
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.3,
+                                   horizontal_flip=True,
+                                   zoom_range=0.3,
+                                   )
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+train_generator = train_datagen.flow_from_directory(train_path,
+                                                    target_size=x.shape[:2],
+                                                    batch_size=batch_size,
+                                                    color_mode="rgb",
+                                                    class_mode="categorical"
+                                                    )
+test_generator = test_datagen.flow_from_directory(train_path,
+                                                  target_size=x.shape[:2],
+                                                  batch_size=batch_size,
+                                                  color_mode="rgb",
+                                                  class_mode="categorical"
+                                                  )
+
+hist=model.fit_generator(
+    generator=train_generator,
+    steps_per_epoch=1600 // batch_size,
+    epochs=100,
+    validation_data=test_generator,
+    validation_steps=400 // batch_size
+)
+model.save_weights("deneme.h5")
+
+print(hist.history.keys())
+plt.plot(hist.history["loss"],label="Train Loss")
+plt.plot(hist.history["val_loss"],label="Validation Loss")
+plt.legend()
+plt.show()
+plt.figure()
+plt.plot(hist.history["accuracy"],label="Train accuracy")
+plt.plot(hist.history["val_accuracy"],label="Validation accuracy")
+plt.legend()
+plt.show()
+#%% save history
+import json
+with open("deneme.json","w") as f:
+    json.dump(hist.history,f)
+
+# Load History
+import codecs
+with codecs.open("cnn_fruit_hist.json","r",encoding="utf-8") as f:
+    h=json.loads(f.read())
+    plt.plot(h["loss"], label="Train Loss")
+    plt.plot(h["val_loss"], label="Validation Loss")
+    plt.legend()
+    plt.show()
+    plt.figure()
+    plt.plot(h["acc"], label="Train acc")
+    plt.plot(h["val_acc"], label="Validation acc")
+    plt.legend()
+    plt.show()
